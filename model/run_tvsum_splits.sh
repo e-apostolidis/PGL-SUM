3c7d073491efbe3d8c5ca0fc81bd71c99d5053a8
Evlampios Apostolidis <apostolid@iti.gr>
2021-08-19 02:08:30 +0300

    Create run_tvsum_splits.sh

---
+++ model/run_tvsum_splits.sh
@@ -0,0 +1,5 @@
+python model/main.py --split_index 0 --n_epochs 200 --batch_size 40 --video_type 'TVSum'
+python model/main.py --split_index 1 --n_epochs 200 --batch_size 40 --video_type 'TVSum'
+python model/main.py --split_index 2 --n_epochs 200 --batch_size 40 --video_type 'TVSum'
+python model/main.py --split_index 3 --n_epochs 200 --batch_size 40 --video_type 'TVSum'
+python model/main.py --split_index 4 --n_epochs 200 --batch_size 40 --video_type 'TVSum'
