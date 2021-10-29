05f08c057f189ff43853622d373573aba758090a
George Balaouras <34791437+mpalaourg@users.noreply.github.com>
2021-10-29 16:25:37 +0300

    Rebase PATHs (Again)

--- inference/inference.py
+++ inference/inference.py
@@ -45,16 +45,16 @@ if __name__ == "__main__":
     # arguments to run the script
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default='SumMe', help="Dataset to be used. Supported: {SumMe, TVSum}")
-    parser.add_argument("--mode", type=str, default='main', help="Mode to be reproduced. Supported: {main, supplementary}")
+    parser.add_argument("--table", type=str, default='4', help="Table to be reproduced. Supported: {3, 4}")
 
     args = vars(parser.parse_args())
     dataset = args["dataset"]
-    mode = args["mode"]
+    table = args["table"]
 
     eval_metric = 'avg' if dataset.lower() == 'tvsum' else 'max'
     for split_id in range(5):
         # Model data
-        model_path = f"../PGL-SUM/inference/pretrained_models/{mode}_models/{dataset}/models/split{split_id}"
+        model_path = f"../PGL-SUM/inference/pretrained_models/table{table}_models/{dataset}/split{split_id}"
         model_file = [f for f in listdir(model_path) if isfile(join(model_path, f))]
 
         # Read current split
